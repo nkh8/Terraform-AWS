60e88756c07dc2f270cf8ad226f256e76b7026bb
nkh8 <mail2nazam@gmail.com>
2022-06-07 17:07:28 +0800

    init

---
+++ AWS-EC2.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0022f774911c1d690"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "NK7AppServerInstance"
+  }
+}
